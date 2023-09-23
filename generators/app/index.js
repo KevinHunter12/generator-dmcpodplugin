ed90c03a67132092617e75979c01be132b853508
KevinHunter12 <kev.hunter@blueyonder.co.uk>
2023-09-23 21:57:42 +0100

    modified due to chalk changes

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
+import chalk from 'chalk';
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
@@ -8,7 +8,7 @@ module.exports = class extends Generator {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the ${chalk.red('SAP Digital Manufacturing Cloud POD Plugin V1.0.9')} generator!`
+        `Welcome to the ${chalk.red('SAP Digital Manufacturing Cloud POD Plugin V1.0.10')} generator!`
       )
     );
 
